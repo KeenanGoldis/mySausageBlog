5ca045996e8b19eefd10033570246c96804a3ea3
unknown <keenangoldis@gmail.com>
2017-12-09 11:07:53 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Keenan's Sausage Blog</title>
+
+    <!--these are my styles-->
+    <link type="text/css" rel="stylesheet" href="css/style.css">
+    <!--this is my font awesome -->
+    <script src="https://use.fontawesome.com/a3bb4c7fa0.js"></script>
+    <!--these are my google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
+    <script type="text/javascript" src="javascript.js"></script>
+  </head>
+  <body>
+    <h1>This is a test</h1>
+    <h1>This is a test</h1>
+    <h1>This is a test</h1>
+    <h1>This is a test</h1>
+
+
+
+  </body>
+</html>
