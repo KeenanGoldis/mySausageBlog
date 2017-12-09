d95f6fe66025f91b40f00607103f4f0a01db7037
unknown <keenangoldis@gmail.com>
2017-12-09 12:25:28 -0600

    copy old project and clean up code

--- index.php
+++ index.php
@@ -5,22 +5,97 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Keenan's Sausage Blog</title>
-
-    <!--these are my styles-->
+  <!--bootstrap CSS-->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <!--these are my styles-->
     <link type="text/css" rel="stylesheet" href="css/style.css">
-    <!--this is my font awesome -->
-    <script src="https://use.fontawesome.com/a3bb4c7fa0.js"></script>
-    <!--these are my google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
-    <script type="text/javascript" src="javascript.js"></script>
   </head>
+
   <body>
-    <h1>This is a test</h1>
-    <h1>This is a test</h1>
-    <h1>This is a test</h1>
-    <h1>This is a test</h1>
+    <header>
+      <nav class="navbar navbar-default navbar-static-top" role="navigation">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#example-navbar-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
 
+            <a class="navbar-brand" href="#">
+            <img alt="brand" src="https://static.independent.co.uk/s3fs-public/styles/story_medium/public/thumbnails/image/2016/10/11/14/beats-logo-1200-80.jpg">
+            </a>
+          </div>
+          <div class="collapse navbar-collapse" id="foodtype-navbar-collapse">
+            <ul class="nav nav-pills pull-right">
+              <li>
+                <a href="#">Mexican</a>
+              </li>
+              <li>
+                <a href="#">Asian</a>
+              </li>
+              <li>
+                <a href="#">Pizza</a>
+              </li>
+              <li>
+                <a href="#">American</a>
+              </li>
+              <li>
+                <a href="#">Vegetarian</a>
+              </li>
+              <li>
+                <a href="#">Moroccan-Artic Fusion</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+<!--START OF MAIN-->
+    <main>
+      <section id="hero" class="container-fluid">
+        <div class="row">
+          <div class="column-md-12">
+            <h1>Order Takeaway or Delivery Food</h1>
+            <p>You should really order food because ordering food is a good decision.</p>
+          </div>
+        </div>
+      </section>
 
+      <section id="options" class="container-fluid">
+        <div class="row icon">
+          <div class="col-md-3 col-sm-6">
+            <img src="img/icon_home_1.svg">
+            <h4>Search by address</h4>
+            <p>Find all restaurants available in your zone</p>
+          </div>
 
+          <div class="col-md-3 col-sm-6">
+            <img src="img/icon_home_2.svg">
+            <h4>Hours of Operation</h4>
+            <p>8am-10pm at all locations!</p>
+          </div>
+
+          <div class="col-md-3 col-sm-6">
+            <img src="img/icon_home_3.svg">
+            <h4>Order Online</h4>
+            <p>Update Credit Card Info</p>
+          </div>
+
+          <div class="col-md-3 col-sm-6">
+            <img src="img/icon_home_4.svg">
+            <h4>Delivery</h4>
+            <p>Get your artisanal biscuits ASAP!</p>
+          </div>
+
+        </div>
+      </section>
+
+    </main>
+    <!-- JQuery CDN script -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!--Bootstrap-->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
   </body>
 </html>
